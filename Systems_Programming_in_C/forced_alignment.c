6c89b98728833a3d14debb42d1ad9acfa6660a42
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-07-09 23:55:58 +0200

    Add files via upload

---
+++ Systems_Programming_in_C/forced_alignment.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+typedef struct Node1{
+	unsigned int a: 6;
+	unsigned int b: 9;
+}	Node1; 
+
+typedef struct Node2{
+	unsigned int a: 6;
+	unsigned int: 0;
+	unsigned int b: 9;
+}	Node2;
+int main(int argc, char * argv[])
+{
+	Node1 node1 = {5,5};
+	Node2 node2 = {2,3};
+
+	int n1_size = sizeof(node1);
+	int n2_size = sizeof(node2);
+
+	printf("Node 1 size %d\n", n1_size);
+	printf("Node 2 size %d\n", n2_size);
+
+}
