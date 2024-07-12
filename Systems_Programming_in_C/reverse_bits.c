be8a65ea3ed8bb4530523120ae093741b2fdfa13
Chrinovic Mukanya <chrinovicmu@gmail.com>
2024-07-12 15:16:43 +0200

    Add files via upload

---
+++ Systems_Programming_in_C/reverse_bits.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(int argc, char argv[])
+{
+    unsigned char byte = 0b10110010;
+    unsigned char rev = 0b10000000;
+    
+    rev = (byte * 0x0202020202ULL & 0x010884422010ULL) % 1023;
+    printf("Original : %d (binary : %08b)\n", byte, byte);
+    printf("Reversed : %d (binary : %08b)\n", rev, rev); 
+
+    unsigned char tx = 0b00001011;
+    printf("Test byte %d\n", tx);
+
+
+}
